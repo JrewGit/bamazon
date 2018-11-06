e251621ba5a6857a2183099fc818e0ed2989e548
Jrew <jrewmohamed@gmail.com>
2018-11-05 20:45:48 -0500

    Added buying functionality and sql updates

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,6 +14,7 @@ connection.connect(function (err) {
     start();
 })
 
+//Calls a function that shows all of the available products inside of the SQL database.
 function start() {
     connection.query('SELECT * FROM products', function (err, res) {
         if (err) throw err;
@@ -23,7 +24,8 @@ function start() {
             console.log(`${res[i].item_id} | ${res[i].product_name} | $${res[i].price}`)
         }
     })
-    //BUG: Prompting before the query is returned
+    //BUG: Prompting before the query is returned.
+    //Calls a function that prompts the user which item they want to buy.
     tobuy();
 }
 
@@ -40,4 +42,33 @@ function tobuy() {
             message: 'How many would you like to buy?'
         }
     ])
+        .then(function (answer) {
+            connection.query('SELECT * FROM products', function (err, res) {
+                if (err) throw err;
+                if (answer.howMany > res[answer.buyer - 1].stock_quantity) {
+                    console.log("Insufficient quantity!")
+                    console.log(`There are only ${res[answer.buyer - 1].stock_quantity} ${res[answer.buyer - 1].product_name} left`);
+                } else {
+                    //Calls a function that updates the amount remaining in the SQL database.
+                    updateQuantity(answer, res);
+                }
+            });
+        })
+
+}
+
+function updateQuantity(answer, res) {
+    connection.query('UPDATE products SET ? WHERE ?',
+        [
+            {
+                stock_quantity: (res[answer.buyer - 1].stock_quantity) - (answer.howMany)
+            },
+            {
+                item_id: answer.buyer
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+            console.log("UPDATED!")
+        })
 }
